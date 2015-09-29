8c25bb4a688e1b0fe236debeae2218e21c361fd2
sebastian.campbell@fao.org <sebastian.campbell@fao.org>
2015-09-29 12:09:28 +0200

    Added package description

---
+++ R/faoswsProduction-package.R
@@ -0,0 +1,13 @@
+##' Repository to contain the code for imputing production data in the 
+##' statistical working system.
+##' 
+##' @name faoswsProduction-package
+##' @aliases faoswsProduction
+##' @docType package
+##' @title Package to perform the imputation of area harvested, production and
+##'   yield for the FAO production domain.
+##' @author Joshua M. Browning \email{joshua.browning@@fao.org}, Michael. C. J. 
+##'   Kao \email{michael.kao@@fao.org}
+##' @keywords package
+##'   
+NULL
