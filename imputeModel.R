763acaf63ed4e5c16dbe786d0ee02a79926f5bbb
rockclimber112358 <rockclimber112358@gmail.com>
2015-10-08 16:33:32 +0200

    Error checking shouldn't be done in try blocks.

--- imputeModel.R
+++ imputeModel.R
@@ -17,7 +17,7 @@ if(!exists("DEBUG_MODE") || DEBUG_MODE == ""){
     ## Define directories
     if(Sys.info()[7] == "josh"){
         apiDirectory = "~/Documents/Github/faoswsProduction/R/"
-        R_SWS_SHARE_PATH = "/media/hqlprsws1_qa/browningj/production"
+        R_SWS_SHARE_PATH = "/media/hqlprsws1_qa/"
         ## R_SWS_SHARE_PATH = "/media/hqlprsws2_prod"
     } else if(Sys.info()[7] == "rockc_000"){
         apiDirectory = "~/Github/faoswsProduction/R/"
@@ -30,7 +30,7 @@ if(!exists("DEBUG_MODE") || DEBUG_MODE == ""){
         ## baseUrl = "https://hqlprswsas1.hq.un.fao.org:8181/sws",
         ## token = "7b588793-8c9a-4732-b967-b941b396ce4d"
         baseUrl = "https://hqlqasws1.hq.un.fao.org:8181/sws",
-        token = "3242f11d-28b2-4429-86b0-6fab97cb50bb"
+        token = "8c65d175-6f8a-4156-b90c-860581e302be"
     )
 
     ## Source local scripts for this local test
@@ -45,17 +45,25 @@ countryM49 = swsContext.datasets[[1]]@dimensions$geographicAreaM49@keys
 stopifnot(startYear <= endYear)
 
 successCount = 0
+failureCount = 0
 
 ## Loop through the items and save production/yield data:
 for(singleItem in swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys){
     
-    impute = try({
-        formulaTuples = getYieldFormula(singleItem)
+    formulaTuples = getYieldFormula(singleItem)
         
-        for(i in 1:nrow(formulaTuples)){
-            ## Verify that the years requested by the user (in swsContext.params) are
-            ## possible based on the constructed model.
-            load(paste0(R_SWS_SHARE_PATH, "prodModel_", singleItem, "_", i, ".RData"))
+    for(i in 1:nrow(formulaTuples)){
+        ## Verify that the years requested by the user (in swsContext.params) are
+        ## possible based on the constructed model.
+        loadData = try({
+            load(paste0(R_SWS_SHARE_PATH, "/browningj/production/prodModel_",
+                        singleItem, "_", i, ".RData"))
+        })
+        if(is(loadData, "try-error")){
+            failureCount = failureCount + 1
+        } else {
+            successCount = successCount + 1
+        
             obsStartYear = min(modelProduction$fit$timePointYears)
             obsEndYear = max(modelProduction$fit$timePointYears)
             if(obsStartYear > startYear){
@@ -65,7 +73,7 @@ for(singleItem in swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys){
             }
             if(obsEndYear < endYear){
                 stop(paste0("Model has been constructed on data that only exists ",
-                            "after the supplied year.  End Year must be at ",
+                            "before the supplied year.  End Year must be at ",
                             "most ", obsEndYear))
             }
             
@@ -91,14 +99,12 @@ for(singleItem in swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys){
             modelYield[, flagMethod := "e"]
             
             dataToSave = rbind(modelYield, modelProduction)
+            dataToSave = dataToSave[!is.na(Value), ]
             SaveData(domain = "agriculture", dataset = "agriculture",
                      data = dataToSave)
         }
-    })
-    if(!is(impute, "try-error"))
-        successCount = successCount + 1
+    }
 }
 
 paste0("Module completed with ", successCount, " successes out of ",
-       length(swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys),
-       " executions")
+       successCount + failureCount, " executions.")
