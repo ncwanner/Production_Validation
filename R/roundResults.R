389b6f4aea9fc53294441a29e3a586fd2675bbad
rockclimber112358 <rockclimber112358@gmail.com>
2016-02-11 13:26:32 +0100

    No rounding should occur in our database

--- R/roundResults.R
+++ R/roundResults.R
@@ -5,18 +5,9 @@
 ##' 
 ##' @param x A numeric value to be rounded.
 ##' 
-##' @result A numeric value after the rounding has occurred.
+##' @return A numeric value after the rounding has occurred.
 ##' 
 
 roundResults = function(x){
-    if(is.na(x)){
-        return(x)
-    }
-    stopifnot(x >= 0)
-    stopifnot(is(x, "numeric"))
-    if(x > 10){
-        return(round(x/10)*10)
-    } else {
-        return(round(x))
-    }
+    return(x)
 }
