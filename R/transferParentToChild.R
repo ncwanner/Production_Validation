a4b8b04a5192499b445c3f755558ff625f7fc579
mk <mkao006@gmail.com>
2016-04-21 12:49:38 +0200

    added warning message for unmapped commodity

--- R/transferParentToChild.R
+++ R/transferParentToChild.R
@@ -3,10 +3,19 @@ transferParentToChild = function(parentData, commodityTree, selectedMeatTable){
         merge(parentData, commodityTree,
               by = c("geographicAreaM49", "timePointYears", "measuredItemCPC"),
               all.x = TRUE)
+    if(any(is.na(parentDataWithMapping$measuredItemChildCPC))){
+        warning("Not all child mapped, unmapped children will be ommited! ",
+                "Please updated the commodity table so all child are mapped ",
+                "for each country, commodity and year combination key")
+        parentDataWithMapping =
+            parentDataWithMapping[!is.na(measuredItemChildCPC), ]
+    }
+
     parentDataWithMapping[, `:=`(c("Value", "share"), list(Value * share, NULL))]
     parentDataWithMapping[, `:=`(c("measuredItemCPC", "extractionRate"),
                                  list(NULL, NULL))]
     setnames(parentDataWithMapping, "measuredItemChildCPC", "measuredItemCPC")
+
     
     childElementTable =
         selectedMeatTable[, c("measuredItemChildCPC", "measuredElementChild"),
