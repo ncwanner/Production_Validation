21744f90a597f21c2090853271e175e83a312f18
mk <mkao006@gmail.com>
2016-04-25 17:23:42 +0200

    added conversion of 0M to NA

--- R/checkTimeSeriesImputed.R
+++ R/checkTimeSeriesImputed.R
@@ -1,9 +1,13 @@
 checkTimeSeriesImputed = function(dataToBeSaved, key, valueColumn){
     ## The number of missing values should be either zero or all
     ## missing.
-    check = dataToBeSaved[, sum(is.na(.SD[[valueColumn]])) == 0 |
-                   sum(is.na(.SD[[valueColumn]])) == .N,
-                 by = c(key)]
+    dataCopy = copy(dataToBeSaved)
+    dataRemoved0M =
+        remove0M(dataCopy, valueVars = valueColumn,
+                 flagVars = "flagObservationStatus")
+    check = dataRemoved0M[, sum(is.na(.SD[[valueColumn]])) == 0 |
+                            sum(is.na(.SD[[valueColumn]])) == .N,
+                          by = c(key)]
     unimputedTimeSeries = which(!check$V1)
     if(length(unimputedTimeSeries) > 0){
         stop("Not all time series are imputed")
