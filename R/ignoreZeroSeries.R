9c312c2ba42dcef9f692caa2d34ee60f8b10ca4b
mk <mkao006@gmail.com>
2016-04-26 13:21:37 +0200

    fixed the issue #73

--- R/ignoreZeroSeries.R
+++ R/ignoreZeroSeries.R
@@ -30,46 +30,48 @@ ignoreZeroSeries = function(d, missingObsFlag = "M", missingMetFlag = "u",
              c("Value", "obsFlag", "metFlag"))
     lastVal = lastVal[!(obsFlag == "M" & metFlag == "u"), ] # Also filters out NA flags
     suppressWarnings({ # Avoid 'Returning -Inf when no value found' warning
-    lastVal[!is.na(Value) & timePointYears <= firstImputeYear, maxYear := max(timePointYears),
-            by = c("geographicAreaM49", "measuredItemCPC")]
+        lastVal[!is.na(Value) & timePointYears <= firstImputeYear, maxYear := max(timePointYears),
+                by = c("geographicAreaM49", "measuredItemCPC")]
     })
-    lastVal = lastVal[timePointYears == maxYear, ]
-    lastVal = lastVal[, list(lastValue = max(Value, na.rm = TRUE)),
-                      by = c("geographicAreaM49", "measuredItemCPC")]
-    lastVal[, stopSeries := is.na(lastValue) | lastValue <= 0]
-    d[lastVal, stopSeries := stopSeries,
-      on = c("geographicAreaM49", "measuredItemCPC")]
-    d[is.na(stopSeries), stopSeries := TRUE] # If we have NAs from merging
-    valCols = grep("Value_", colnames(d), value = TRUE)
-    obsFlagCols = grep("flagObservation", colnames(d), value = TRUE)
-    metFlagCols = grep("flagMethod", colnames(d), value = TRUE)
-    postfix = gsub("Value", "", valCols)
-    
-    ## If any value is 0M-, stop the series
-    filter = lapply(postfix, function(code){
-        d[, (is.na(get(paste0("Value", code))) |
+    if("maxYear" %in% colnames(lastVal)){
+        lastVal = lastVal[timePointYears == maxYear, ]
+        lastVal = lastVal[, list(lastValue = max(Value, na.rm = TRUE)),
+                          by = c("geographicAreaM49", "measuredItemCPC")]
+        lastVal[, stopSeries := is.na(lastValue) | lastValue <= 0]
+        d[lastVal, stopSeries := stopSeries,
+          on = c("geographicAreaM49", "measuredItemCPC")]
+        d[is.na(stopSeries), stopSeries := TRUE] # If we have NAs from merging
+        valCols = grep("Value_", colnames(d), value = TRUE)
+        obsFlagCols = grep("flagObservation", colnames(d), value = TRUE)
+        metFlagCols = grep("flagMethod", colnames(d), value = TRUE)
+        postfix = gsub("Value", "", valCols)
+        
+        ## If any value is 0M-, stop the series
+        filter = lapply(postfix, function(code){
+            d[, (is.na(get(paste0("Value", code))) |
                  get(paste0("Value", code)) == 0) &
-            sapply(get(paste0("flagObservationStatus", code)), identical, y = "M") &
-            sapply(get(paste0("flagMethod", code)), identical, y = "-")]
-    })
-    ## May be a better way to do this:
-    filter = apply(do.call("cbind", filter), 1, any)
-    d[, stopSeries := stopSeries | filter]
-    
-    sapply(postfix, function(p){
-        d[(stopSeries),
-                c(paste0(c("Value", "flagObservationStatus", "flagMethod"), p)) :=
-                    list(0, "E", "-")]
-        d[is.na(get(paste0("Value", p))) & !(stopSeries),
-                c(paste0(c("Value", "flagObservationStatus", "flagMethod"), p)) :=
-                    list(0, "M", "u")]
+                sapply(get(paste0("flagObservationStatus", code)), identical, y = "M") &
+                sapply(get(paste0("flagMethod", code)), identical, y = "-")]
         })
-#     sapply(obsFlagCols, function(colname){
-#         d[is.na(get(colname)) & !stopSeries, c(colname) := "M"]})
-#     sapply(metFlagCols, function(colname){
-#         d[is.na(get(colname)) & stopSeries , c(colname) := "-"]})
-#     sapply(metFlagCols, function(colname){
-#         d[is.na(get(colname)) & !stopSeries, c(colname) := "u"]})
-#         d[is.na(get(colname)), c(colname) := 0]})
+        ## May be a better way to do this:
+        filter = apply(do.call("cbind", filter), 1, any)
+        d[, stopSeries := stopSeries | filter]
+        
+        sapply(postfix, function(p){
+            d[(stopSeries),
+              c(paste0(c("Value", "flagObservationStatus", "flagMethod"), p)) :=
+              list(0, "E", "-")]
+            d[is.na(get(paste0("Value", p))) & !(stopSeries),
+              c(paste0(c("Value", "flagObservationStatus", "flagMethod"), p)) :=
+              list(0, "M", "u")]
+        })
+        ##     sapply(obsFlagCols, function(colname){
+        ##         d[is.na(get(colname)) & !stopSeries, c(colname) := "M"]})
+        ##     sapply(metFlagCols, function(colname){
+        ##         d[is.na(get(colname)) & stopSeries , c(colname) := "-"]})
+        ##     sapply(metFlagCols, function(colname){
+        ##         d[is.na(get(colname)) & !stopSeries, c(colname) := "u"]})
+        ##         d[is.na(get(colname)), c(colname) := 0]})
+    }
     d[, stopSeries := NULL]
 }
