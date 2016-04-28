3b2ad79ba87c523e142375a250237ff5a3bea12a
mk <mkao006@gmail.com>
2016-04-28 12:36:58 +0200

    Major change, now return full dataset as the same dimension of the input data rather than the 'updated observation'. selection of the 'updated observation' should be done outside the imputation. Also removed the function cleanData which was the root source why official figures were overwritten and also non-sensible expansion of data

--- R/imputeMeatTriplet.R
+++ R/imputeMeatTriplet.R
@@ -3,7 +3,6 @@ imputeMeatTriplet = function(meatKey, minObsForEst = 5){
     datasets = getProductionData(meatKey)
     ## Ignore indigenous/biological:
     datasets$formulaTuples = datasets$formulaTuples[nchar(input) == 4, ]
-
     ## NOTE (Michael): Sometimes there are no data in the database,
     ##                 this case, we simply return an empty data table.
     if(NROW(datasets$query) > 0){
@@ -16,14 +15,14 @@ imputeMeatTriplet = function(meatKey, minObsForEst = 5){
             ## For convenience, let's save the formula tuple to "formula"
             message("Processing pair ", i, " of ", nrow(datasets$formulaTuples),
                     " element triples.")
-            cleanedData = cleanData(datasets, i = i, maxYear = 2014)
+            ## cleanedData = cleanData(datasets, i = i, maxYear = 2014)
             warning("The hard coded year in this function should be removed!!!")
 
 
             ## Split the data for easy reference
-            currentData = cleanedData$query
-            currentFormula = cleanedData$formulaTuples[i, ]
-            currentPrefix = cleanedData$prefixTuples
+            currentData = datasets$query
+            currentFormula = datasets$formulaTuples[i, ]
+            currentPrefix = datasets$prefixTuples
 
             ## Setup for the imputation
             processingParams =
@@ -63,12 +62,14 @@ imputeMeatTriplet = function(meatKey, minObsForEst = 5){
             ## enough official/semi-official data.  "Enough" is specified by
             ## minObsForEst.
             flags = paste0(currentPrefix$flagObsPrefix,
-                           currentFormula[, c("productivity", "output"), with = FALSE])
+                           currentFormula[, c("input", "productivity", "output"),
+                                          with = FALSE])
 
             validObsCnt =
                 useEstimateForTimeSeriesImputation(data = removedSingleEntryData,
-                                                   yieldObsFlagVar = flags[1],
-                                                   prodObsFlagVar = flags[2],
+                                                   areaObsFlagVar = flags[1],
+                                                   yieldObsFlagVar = flags[2],
+                                                   prodObsFlagVar = flags[3],
                                                    minObsForEst = minObsForEst)
 
             ## For the actual imputation, we must pass all the data
@@ -106,60 +107,73 @@ imputeMeatTriplet = function(meatKey, minObsForEst = 5){
             ## include I/i (as balanced observations may not be correct, because
             ## we could use estimates for yield imputation and not for
             ## production, for example).
-            valuesImputed1 =
-                getUpdatedObs(dataOld = origData,
-                              dataNew = imputation1,
-                              key = c("geographicAreaM49", "timePointYears"),
-                              wideVarName = "measuredElement")
+            ## valuesImputed1 =
+            ##     getUpdatedObs(dataOld = origData,
+            ##                   dataNew = imputation1,
+            ##                   key = c("geographicAreaM49", "timePointYears"),
+            ##                   wideVarName = "measuredElement")
             ## Formula to only include series with enough data:
+            imputationNormalised1 = normalise(imputation1)
             valuesImputedWithoutEstimates =
-                getImputedValues(data = valuesImputed1,
+                selectUseEstimate(data = imputationNormalised1,
                                  useEstimatesTable = validObsCnt,
                                  useEstimates = FALSE)
 
             ## Now add in the values imputed in the second round
-            valuesImputed2 =
-                getUpdatedObs(dataOld = origData,
-                              dataNew = imputation2,
-                              key = c("geographicAreaM49", "timePointYears"),
-                              wideVarName = "measuredElement")
+            ## valuesImputed2 =
+            ##     getUpdatedObs(dataOld = origData,
+            ##                   dataNew = imputation2,
+            ##                   key = c("geographicAreaM49", "timePointYears"),
+            ##                   wideVarName = "measuredElement")
+            imputationNormalised2 = normalise(imputation2)
             valuesImputedWithEstimates =
-                getImputedValues(data = valuesImputed2,
+                selectUseEstimate(data = imputationNormalised2,
                                  useEstimatesTable = validObsCnt,
                                  useEstimates = TRUE)
 
             ## Bring together the estimates and reshape them:
             valuesImputed = combineImputation(valuesImputedWithoutEstimates,
-                                              valuesImputedWithEstimates)
-            updatedData =
-                updateOriginalDataWithImputation(originalData = origData,
-                                                 imputedData = valuesImputed,
-                                                 yieldElementCode =
-                                                     currentFormula[, productivity],
-                                                 prodElementCode =
-                                                     currentFormula[, output])
+                                                valuesImputedWithEstimates)
+            ## updatedData <<-
+            ##     updateOriginalDataWithImputation(originalData = origData,
+            ##                                      imputedData = valuesImputed,
+            ##                                      yieldElementCode =
+            ##                                          currentFormula[, productivity],
+            ##                                      prodElementCode =
+            ##                                          currentFormula[, output])
+
+            ## NOTE (Michael): The reason why we have to rebalance the
+            ##                 imputation is due to the fact that the
+            ##                 imputation comes from 2 different
+            ##                 process.
+            ##
+            ##                 The hack of switching between
+            ##                 imputation with/without manual
+            ##                 'E'stimates will eventually be removed.
+            unbalancedImputation =
+                denormalise(valuesImputed, denormaliseKey = "measuredElement")
             ## Now, use the identity Yield = Production / Area to add in missing
             ## values.
-            computeYield(data = updatedData,
+            computeYield(data = unbalancedImputation,
                          processingParameters = processingParams,
                          unitConversion = currentFormula[, unitConversion])
-            balanceProduction(data = updatedData,
+            balanceProduction(data = unbalancedImputation,
                               processingParameters = processingParams,
                               unitConversion = currentFormula[, unitConversion])
-            balanceAreaHarvested(data = updatedData,
+            balanceAreaHarvested(data = unbalancedImputation,
                                  processingParameters = processingParams,
                                  unitConversion = currentFormula[, unitConversion])
 
             ## Remove the observations we don't want to impute on
             ## Use keys so we can do an anti-join
-            removedZeroData = removeForcedZero(data = updatedData,
-                                               forcedZeroKey = forcedZero)
-            imputedData = filterTimeRange(data = removedZeroData,
-                                          firstYear = firstYear,
-                                          lastYear = lastYear)
-            setkeyv(imputedData,
-                    cols = c("geographicAreaM49", "measuredItemCPC", "timePointYears"))
-            finalData = merge(finalData, imputedData, by = key(finalData), all.x = TRUE)
+            ## removedZeroData = removeForcedZero(data = unbalancedImputation,
+            ##                                      forcedZeroKey = forcedZero)
+            setkeyv(unbalancedImputation,
+                    cols = c("geographicAreaM49", "measuredItemCPC",
+                             "timePointYears"))
+
+            finalData = merge(finalData, unbalancedImputation, by = key(finalData),
+                              all.x = TRUE)
         } ## close item type for loop
     } else {
         finalData = datasets$query
