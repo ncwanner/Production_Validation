dc7d5b43f8be1e22fdc72e106271a6009b81d373
mk <mkao006@gmail.com>
2016-05-04 17:20:06 +0200

    added default year and also moved functions to package

--- impute_missing_values.R
+++ impute_missing_values.R
@@ -43,13 +43,13 @@ if(CheckDebug()){
             SetClientFiles("~/.R/qa/")
             R_SWS_SHARE_PATH = "/media/sws_qa_shared_drive"
             apiDirectory = "R/"
-        }        
+        }
         GetTestEnvironment(
             baseUrl = "https://hqlqasws1.hq.un.fao.org:8181/sws",
             ## token = "f8646896-2ed2-4e88-9cd2-9db6d735991f"
             ## New Token for all the meats
             token = "732603a1-f4ea-40d8-8858-b31e41b0092c"
-        )        
+        )
     } else {
         stop("Please specify a valid server name")
     }
@@ -62,39 +62,33 @@ if(CheckDebug()){
 modelLoadingPath = paste0(R_SWS_SHARE_PATH, "/kao/production/imputation_fit/")
 selectedKey = swsContext.datasets[[1]]
 
-## NOTE (Michael): The selected item should be based on the main key.
-completeImputationKey = getMainKey(1992:2014)
+
+## NOTE (Michael): The imputation and all modules should now have a base year of
+##                 1999, this is the result of a discussion with Pietro.
+defaultYear = 1999
+imputationYears =
+    GetCodeList(domain = "agriculture",
+                dataset = "aproduction",
+                dimension = "timePointYears") %>%
+    filter(description != "wildcard" & as.numeric(code) >= defaultYear) %>%
+    select(code) %>%
+    unlist(use.names = FALSE)
+
+
+## Get the complete set of keys for imputation
+completeImputationKey = getMainKey(imputationYears)
+
+## Subset the item of the complete key to the selected items. We use the
+## complete key to subset as the selection may not include the full list of
+## year/country/elements
+selectedImputationItems = selectImputationItem(selectedKey, completeImputationKey)
 
 ## Test whether the selected keys are in the complete key, if not in
 ## the completeImputationKey, then it will be returned as a message
 ## that the item should probably be added in.
-nonImputationItems = function(selectedKey, imputationKey){
-    selectedItems = selectedKey@dimensions$measuredItemCPC@keys
-    imputationItems = imputationKey@dimensions$measuredItemCPC@keys
-    nonImputationItems = setdiff(selectedItems, imputationItems)
-    nonImputationItems
-}
-
-
-imputationExist = function(modelLoadingPath, item, modelPrefix = "imputation_"){
-    modelName = paste0("imputation_", item, ".rds")
-    file.exists(paste0(modelLoadingPath, modelName))
-}
-
-selectImputationItem = function(selectedKey, imputationKey){
-    selectedItems = selectedKey@dimensions$measuredItemCPC@keys
-    imputationItems = imputationKey@dimensions$measuredItemCPC@keys
-    selectedImputationItems = intersect(selectedItems, imputationItems)
-    selectedImputationItems
-}
-
-isPrimary = function(itemCode, primaryPattern = "^0"){
-    grepl(primaryPattern, itemCode)
-}
-
-selectedImputationItems = selectImputationItem(selectedKey, completeImputationKey)
-
 nonImputationItemCodes = nonImputationItems(selectedKey, completeImputationKey)
+
+selectedImputationItems = "01371"
 
 ## Start the imputation, looping through each item since the model are
 ## fitted for each item separately.
@@ -117,14 +111,15 @@ for(i in seq(selectedImputationItems)){
     ##                 object. Probably need a function to standardise
     ##                 and sync this with the create_imputed_dataset
     ##                 module.
-    modelName = paste0("imputation_", currentItem, ".rds")
+    modelName = createImputationObjectName(item = currentItem)
 
     ## Modify the selected key
     subKey = selectedKey
     subKey@dimensions$measuredItemCPC@keys = currentItem
     subKey@dimensions$measuredElement@keys = currentElements
 
-    print(paste0("Imputation for item: ", currentItem, " (",  i, " out of ",
+    print(paste0("Filling missing values for item: ",
+                 currentItem, " (",  i, " out of ",
                  length(selectedImputationItems),")"))
 
     ## Load the fitted values
@@ -134,21 +129,19 @@ for(i in seq(selectedImputationItems)){
         filter(flagObservationStatus == "I" & flagMethod %in% c("i", "e")) %>%
         setkeyv(x = ., col = c("geographicAreaM49", "measuredItemCPC",
                                "timePointYears", "measuredElement"))
-    
-    
+
     ## Load the selected data from the data base
     currentValues =
         GetData(subKey) %>%
         preProcessing(data = .) %>%
         setkeyv(x = , col = c("geographicAreaM49", "measuredItemCPC",
                               "timePointYears", "measuredElement"))
-    
 
     ## NOTE (Michael): Need to think about what should be
     ##                 updated. Should we over-write all the data with
     ##                 specific set of flags, or only values that are
     ##                 missing?
-    
+
     saveResult =
         {
             ## NOTE (Michael): Only impute production if the item is
