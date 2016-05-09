8def6d5437d215933575a6432af47607df23b54f
mk <mkao006@gmail.com>
2016-05-09 17:06:14 +0200

    moved scattered modules to centralised location

---
+++ modules/build_imputed_dataset/main.R
@@ -0,0 +1,179 @@
+## load the library
+suppressMessages({
+    library(faosws)
+    library(faoswsUtil)
+    library(faoswsFlag)
+    library(faoswsImputation)
+    library(magrittr)
+    library(dplyr)
+    ## TODO (Michael): The following packages should be removed when the
+    ##                 \pkgs{faoswsProduction} package is ready and the module
+    ##                 no longer depends on sourcing the R/ folder.
+    ##
+    ## library(faoswsProduction)
+    library(data.table)
+    library(splines)
+})
+
+## set up for the test environment and parameters
+R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
+savePath = paste0(R_SWS_SHARE_PATH, "/kao/production/imputation_fit/")
+
+## This return FALSE if on the Statistical Working System
+if(CheckDebug()){
+
+    library(faoswsModules)
+    SETTINGS = ReadSettings("sws.yml")
+
+    ## If you're not on the system, your settings will overwrite any others
+    R_SWS_SHARE_PATH = SETTINGS[["share"]]
+
+    ## Define where your certificates are stored
+    SetClientFiles(SETTINGS[["certdir"]])
+
+    ## Get session information from SWS. Token must be obtained from web interface
+    GetTestEnvironment(baseUrl = SETTINGS[["server"]],
+                       token = SETTINGS[["token"]])
+
+    savePath = SETTINGS[["save_imputation_path"]]
+}
+
+## NOTE (Michael): The imputation and all modules should now have a base year of
+##                 1999, this is the result of a discussion with Pietro.
+defaultYear = 1999
+imputationYears =
+    GetCodeList(domain = "agriculture",
+                dataset = "aproduction",
+                dimension = "timePointYears") %>%
+    filter(description != "wildcard" & as.numeric(code) >= defaultYear) %>%
+    select(code) %>%
+    unlist(use.names = FALSE)
+
+## Get the required Datakey
+completeImputationKey = getMainKey(years = imputationYears)
+selectedItemCode = completeImputationKey@dimensions[["measuredItemCPC"]]@keys
+
+for(iter in 1:length(selectedItemCode)){
+    currentItem = selectedItemCode[iter]
+    subKey = completeImputationKey
+    subKey@dimensions$measuredItemCPC@keys = currentItem
+
+    ## Obtain the formula and remove indigenous and biological meat.
+    ##
+    ## NOTE (Michael): Biological and indigenous meat are currently removed, as
+    ##                 they have incorrect data specification. They should be
+    ##                 separate item with different item code rather than under
+    ##                 different element under the meat code.
+    currentFormula =
+        getYieldFormula(currentItem) %>%
+        removeIndigenousBiologicalMeat(formula = .)
+
+    ## Update the element corresponding to the current item
+    subKey@dimensions$measuredElement@keys =
+        currentFormula[, unlist(.(input, productivity, output))]
+
+    ## Create processing parameters
+    processingParams =
+        defaultProcessingParameters(productionValue = currentFormula$output,
+                                    areaHarvestedValue = currentFormula$input,
+                                    yieldValue = currentFormula$productivity)
+
+    print(paste0("Ensuring data consistency for item: ", currentItem, " (",
+                 iter, " out of ", length(selectedItemCode),")"))
+
+    ## NOTE (Michael): This is the data cleaning step, and official/semi-official
+    ##                 flags can be over-written when in conflict are inconsistent.
+    ##
+    ## TODO (Michael): This should be probably be splitted into a seperate
+    ##                 module.
+    subKey %>%
+        GetData(.) %>%
+        preProcessing(data = .,
+                      params = processingParams) %>%
+        denormalise(normalisedData = .,
+                    denormaliseKey = "measuredElement") %>%
+        removeZeroYield(data = .,
+                        yieldValue = processingParams$yieldValue,
+                        yieldObsFlag = processingParams$yieldObservationFlag,
+                        yieldMethodFlag = processingParams$yieldMethodFlag) %>%
+        removeZeroConflict(data = .,
+                           value1 = processingParams$productionValue,
+                           value2 = processingParams$areaHarvestedValue,
+                           observationFlag1 =
+                               processingParams$productionObservationFlag,
+                           observationFlag2 =
+                               processingParams$areaHarvestedObservationFlag,
+                           methodFlag1 = processingParams$productionMethodFlag,
+                           methodFlag2 =
+                               processingParams$areaHarvestedMethodFlag) %>%
+        normalise(denormalisedData = .) %>%
+        postProcessing(data = .,
+                       params = processingParams) %>%
+        SaveData(domain = "agriculture",
+                 dataset = "aproduction",
+                 data = .)
+
+
+
+    ## Print message, initialise the save name and start the imputation.
+    print(paste0("Imputation for item: ", currentItem, " (",  iter, " out of ",
+                 length(selectedItemCode),")"))
+    saveFileName = createImputationObjectName(item = currentItem)
+
+    ## NOTE (Michael): We now impute the full triplet rather than
+    ##                 just production for non-primary
+    ##                 products. However, in the imputeModel
+    ##                 module, only the production will be
+    ##                 selected and imputed for non-primary
+    ##                 products.
+    ##
+    ##                 Nevertheless, we hope to change this and
+    ##                 impute the full triplet, for all the
+    ##                 commodity. We will need to transfer and
+    ##                 sync the inputs just like in the meat
+    ##                 case. This will allow us to merge the two
+    ##                 components.
+    ##
+    imputation = try({
+        imputed = imputeMeatTriplet(meatKey = subKey)
+
+        ## Check the imputation before saving.
+        imputed %>%
+            checkProductionBalanced(dataToBeSaved = .,
+                                    areaVar = processingParams$areaHarvestedValue,
+                                    yieldVar = processingParams$yieldValue,
+                                    prodVar = processingParams$productionValue,
+                                    conversion = currentFormula$unitConversion) %>%
+            normalise(.) %>%
+            postProcessing(data = .) %>%
+            checkTimeSeriesImputed(dataToBeSaved = .,
+                                   key = c("geographicAreaM49",
+                                           "measuredItemCPC",
+                                           "measuredElement"),
+                                   valueColumn = "Value") %>%
+            postProcessing(data = ., params = processingParams) %>%
+            {
+                ## HACK (Michael): Before we decide how to deal with the flags,
+                ##                 we will not perform this check as we can not
+                ##                 determine what is considered 'protected'
+                ##
+                ## Check whether protected data are being over-written
+                ## filter(.data = ., flagMethod %in% c("i", "e")) %>%
+                ##     checkProtectedData(dataToBeSaved = .) %>%
+                ##     print(.)
+
+                ## Save the fitted object for future loading
+                saveRDS(object = ., file = paste0(savePath, saveFileName))
+            }
+
+    })
+
+    if(!inherits(imputation, "try-error")){
+        message("Imputation module completed successfully")
+    } else {
+        stop(paste0("Imputation moduled failed at item ", currentItem,
+                    " with the following error:\n", imputation[1]))
+    }
+}
+
+
---
+++ modules/impute_slaughtered/main.R
@@ -0,0 +1,220 @@
+## ###########################################################################
+## Title: Impute Slaughtered Module for SWS
+##
+## Author: Josh Browning
+##
+## The animals slaughtered for production of meat, offals, fats and hides must
+## be available before running the production imputation code.  These numbers,
+## however, are not guaranteed to be available, and in the case of missing data,
+## an imputation method must be applied.  The decision was to use the production
+## figures of meat, if available, to compute the missing animals slaughtered. If
+## these figures are also missing, they should be imputed using the production
+## imputation methodology.  Of course, in the case of currently available data
+## in the animal element, that data should be transferred to the quantity of
+## animals slaughtered for meat and then the imputation ran.  We also decided to
+## save the imputations for meat so as to retain consistency with the animal
+## figures.
+##
+## The steps are as follows:
+## 0. Transfer down the slaughtered animal numbers from the animal (parent)
+## commodity to the meat (child) commodity.
+## 1. Save the transfered data back to the database
+## 2. Impute the meat data (production/animals slaughtered/carcass weight)
+## following the logic from the production imputation module.
+## 3. Copy the slaughtered animal numbers in meat back to the animal commodity.
+## 4. Save all three variables for meat (production/animals slaughterd/carcass
+## weight) and the animals slaughtered for the animal.
+## #############################################################################
+
+## Step 0. Initial set-up
+
+cat("Beginning impute slaughtered script...\n")
+suppressMessages({
+    library(data.table)
+    library(faosws)
+    library(faoswsFlag)
+    library(faoswsUtil)
+    library(faoswsImputation)
+    library(splines)
+    library(magrittr)
+    library(dplyr)
+})
+
+minObsForEst = 5
+yearsModeled = 20
+
+## set up for the test environment and parameters
+R_SWS_SHARE_PATH = Sys.getenv("R_SWS_SHARE_PATH")
+
+## This return FALSE if on the Statistical Working System
+if(CheckDebug()){
+
+    library(faoswsModules)
+    SETTINGS = ReadSettings("sws.yml")
+
+    ## If you're not on the system, your settings will overwrite any others
+    R_SWS_SHARE_PATH = SETTINGS[["share"]]
+
+    ## Define where your certificates are stored
+    SetClientFiles(SETTINGS[["certdir"]])
+
+    ## Get session information from SWS. Token must be obtained from web interface
+    GetTestEnvironment(baseUrl = SETTINGS[["server"]],
+                       token = SETTINGS[["token"]])
+
+}
+
+## Just testing 1 item
+## swsContext.datasets[[1]]@dimensions[[measuredItemCPC]]@keys = "21111.01" ## meat of cattle
+## swsContext.datasets[[1]]@dimensions[[measuredItemCPC]]@keys = "21113.01" ## meat of pig
+
+startTime = Sys.time()
+
+message("Loading preliminary data...\n")
+firstYear = as.numeric(swsContext.computationParams$firstYear)
+lastYear = as.numeric(swsContext.computationParams$lastYear)
+firstDataYear = lastYear - yearsModeled + 1
+stopifnot(firstDataYear <= firstYear)
+stopifnot(firstYear <= lastYear)
+
+selectedMeatTable =
+    getAnimalMeatMapping(R_SWS_SHARE_PATH = R_SWS_SHARE_PATH,
+                         onlyMeatChildren = TRUE) %>%
+    select(measuredItemParentCPC, measuredElementParent,
+           measuredItemChildCPC, measuredElementChild)
+
+## Read the data.  The years and countries provided in the session are
+## used, and the commodities in the session are somewhat
+## considered. For example, if 02111 (Cattle) is in the session, then
+## the session will be expanded to also include 21111.01 (meat of
+## cattle, fresh or chilled), 21151 (edible offal of cattle, fresh,
+## chilled or frozen), 21512 (cattle fat, unrendered), and 02951.01
+## (raw hides and skins of cattle).  The measured element dimension of
+## the session is simply ignored.
+
+## Expand the session to include missing meats
+newKey =
+    expandMeatSessionSelection(oldKey = swsContext.datasets[[1]],
+                               selectedMeatTable = selectedMeatTable)
+
+## Adjust the years based on the passed information:
+newKey@dimensions[["timePointYears"]]@keys =
+    as.character(firstDataYear:lastYear)
+
+## Include all countries, since all data is required for the imputation
+countryCodes = GetCodeList("agriculture", "aproduction", "geographicAreaM49")
+newKey@dimensions[["geographicAreaM49"]]@keys =
+    countryCodes[type == "country", code]
+
+## Execute the get data call.
+cat("Pulling the data...\n")
+
+step1Data =
+    newKey %>%
+    GetData(key = .) %>%
+    preProcessing(data = .) %>%
+    ## TODO (Michael): Should add preProcessing here
+    transferAnimalNumber(data = ., selectedMeatTable)
+
+## Module test and save the transfered data back
+step1Data %>%
+    postProcessing(data = .) %>%
+    checkProtectedData(dataToBeSaved = .) %>%
+    SaveData("agriculture", "aproduction", data = .)
+
+
+
+## Step 2. Impute the meat data (production/animals
+##         slaughtered/carcass weight) following the logic from the
+##         production imputation module.
+##
+## NOTE (Michael): The imputed data for meat triplet is also saved
+##                 back in this step.
+
+selectedMeatCode =
+    getSessionMeatSelection(key = newKey,
+                            selectedMeatTable = selectedMeatTable)
+
+result = NULL
+successCount = 0
+failCount = 0
+for(iter in 1:length(selectedMeatCode)){
+    currentMeat = selectedMeatCode[iter]
+    subKey = newKey
+    subKey@dimensions$measuredItemCPC@keys = currentMeat
+    print(paste0("Imputation for item: ", currentMeat, " (",  iter, " out of ",
+                 length(selectedMeatCode),")"))
+
+    imputed = imputeMeatTriplet(meatKey = subKey)
+
+
+    if(inherits(imputed, "try-error")){
+        message("Imputation Module Failed!")
+        failCount = failCount + 1
+    } else {
+        successCount = successCount + 1
+        ## New module test
+        imputed %>%
+            normalise(.) %>%
+            ## Change time point year back to character
+            postProcessing(data = .) %>%
+            checkTimeSeriesImputed(dataToBeSaved = .,
+                                   key = c("geographicAreaM49",
+                                           "measuredItemCPC",
+                                           "measuredElement"),
+                                   valueColumn = "Value") %>%
+            checkProtectedData(dataToBeSaved = .) %>%
+            SaveData(domain = "agriculture", dataset = "aproduction", data = .)
+
+
+        message("Imputation Module Executed Successfully!")
+        ## Just need to return the numbers slaughtered code:
+        ##
+        ## TODO (Michael): Need to restructure the get formula
+        formulaTuples =
+            getYieldFormula(slot(slot(subKey,
+                                      "dimensions")$measuredItemCPC, "keys"))
+        formulaTuples = formulaTuples[nchar(input) == 4, ]
+        slaughteredAnimal =
+            getSlaughteredAnimal(data = imputed,
+                                 formulaTuples = formulaTuples)
+        result = rbind(result, slaughteredAnimal)
+    }
+}
+
+
+if(!is.null(result)){
+    saveResult =
+        ## Step 3. Copy the slaughtered animal numbers in meat back to the
+        ##         animal commodity.
+        step1Data %>%
+        transferSlaughteredNumber(preUpdatedData = .,
+                                  imputationResult = result,
+                                  selectedMeatTable = selectedMeatTable) %>%
+        ## Post process the data
+        postProcessing(data = .) %>%
+        ## Module Testing before saving the data back to the database
+        checkTimeSeriesImputed(dataToBeSaved = .,
+                               key = c("geographicAreaM49",
+                                       "measuredItemCPC", "measuredElement"),
+                               valueColumn = "Value") %>%
+        checkProtectedData(dataToBeSaved = .) %>%
+        ## Step 4. Save all three variables for meat (production/animals
+        ##         slaughterd/carcass weight) and the animals slaughtered
+        ##         for the animal.
+        ##
+        ## Note (Michael): The above comment is incorrect, only the animal
+        ##                 number is saved back to the animal commdotiy.
+        SaveData(domain = "agriculture", dataset = "aproduction",
+                 data = .)
+
+    ## if(!inherits(moduleTest3, "try-error"))
+    ##     saveResult = SaveData(domain = "agriculture", dataset = "aproduction",
+    ##                           data = data)
+}
+
+message = paste("Module completed with", successCount,
+                "commodities imputed out of", successCount + failCount)
+message(message)
+
+message
