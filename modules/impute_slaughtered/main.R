d924dd27e46c0cf4428862d5ba85c063a7fa7fe7
mk <mkao006@gmail.com>
2016-05-14 17:58:55 +0200

    separated processing from imputation

--- modules/impute_slaughtered/main.R
+++ modules/impute_slaughtered/main.R
@@ -65,6 +65,9 @@ startTime = Sys.time()
 
 ("Loading preliminary data...\n")
 sessionKey = swsContext.datasets[[1]]
+datasetConfig = GetDatasetConfig(domainCode = sessionKey@domain,
+                                 datasetCode = sessionKey@dataset)
+
 
 ## NOTE (Michael): The imputation and all modules should now have a base year of
 ##                 1999, this is the result of a discussion with Pietro.
@@ -143,52 +146,111 @@ selectedMeatCode =
                             selectedMeatTable = selectedMeatTable)
 
 
-result = NULL
+updatedSlaughteredAnimal = NULL
 for(iter in seq(selectedMeatCode)){
     currentMeat = selectedMeatCode[iter]
     subKey = expandedMeatKey
     subKey@dimensions$measuredItemCPC@keys = currentMeat
 
-    print(paste0("Imputation for item: ", currentMeat, " (",  iter, " out of ",
-                 length(selectedMeatCode),")"))
-
-    imputed = imputeWithAndWithoutEstimates(meatKey = subKey)
-
-    ## New module test
-    imputed %>%
-        normalise(.) %>%
-        postProcessing(data = .) %>%
-        checkTimeSeriesImputed(dataToBeSaved = .,
-                               key = c("geographicAreaM49",
-                                       "measuredItemCPC",
-                                       "measuredElement"),
-                               valueColumn = "Value") %>%
-        filter(flagMethod %in% c("i", "t", "e", "n", "u")) %>%
-        ## NOTE (Michael): This test currently fails because it can not
-        ##                 overwrite the ('I', '-') flag for imputed value
-        ##                 from the old system. This is an error in the
-        ##                 system as the flag ('I', '-') should be replaced
-        ##                 with ('E', 'e') which can be over-written.
-        checkProtectedData(dataToBeSaved = .) %>%
-        SaveData(domain = subKey@domain, dataset = subKey@dataset, data = .)
+    cat("Imputation for item: ", currentMeat, " (",  iter, " out of ",
+        length(selectedMeatCode),")\n")
 
 
-
-    ## Just need to return the numbers slaughtered code:
+    ## Get all the formula associated with the item
     ##
-    ## TODO (Michael): Need to restructure the get formula
-    formulaTuples =
-        getYieldFormula(slot(slot(subKey,
-                                  "dimensions")$measuredItemCPC, "keys")) %>%
+    ## NOTE (Michael): Biological and indigenous meat are currently removed, as
+    ##                 they have incorrect data specification. They should be
+    ##                 separate item with different item code rather than under
+    ##                 different element under the meat code.
+    allFormula =
+        getYieldFormula(itemCode = currentMeat) %>%
         removeIndigenousBiologicalMeat(formula = .)
 
-    slaughteredAnimal =
-        getSlaughteredAnimal(data = imputed,
-                             formulaTuples = formulaTuples)
-    result = rbind(result, slaughteredAnimal)
+    for(j in 1:nrow(allFormula)){
+        currentFormula = allFormula[j, ]
+        with(currentFormula,
+             cat("Imputation for formula: ", output, " = ", input, " x ",
+                 productivity, " (", j, " out of ", nrow(allFormula), ")\n"))
+
+        ## Update the element code according to the current formula
+        subKey@dimensions$measuredElement@keys =
+            currentFormula[, unlist(.(input, productivity, output))]
+
+        ## Build processing parameters
+        ##
+        ## TODO (Michael): Split this into general processing parameters, and
+        ##                 yield formula parameters (or can put into the
+        ##                 imputation parameters)
+        processingParameters =
+            productionProcessingParameters(
+                datasetConfig = datasetConfig,
+                productionCode = currentFormula$output,
+                areaHarvestedCode = currentFormula$input,
+                yieldCode = currentFormula$productivity,
+                unitConversion = currentFormula$unitConversion)
+
+        ## Build imputation parameter
+        imputationParameters =
+            getImputationParameters(productionCode = currentFormula$output,
+                                    areaHarvestedCode = currentFormula$input,
+                                    yieldCode = currentFormula$productivity)
+
+        ## Process the data.
+        processedData =
+            GetData(subKey) %>%
+            fillRecord(data = .) %>%
+            preProcessing(data = .) %>%
+            denormalise(normalisedData = ., denormaliseKey = "measuredElement") %>%
+            processProductionDomain(data = .,
+                                    processingParameters = processingParameters)
+
+        ## Perform imputation
+        imputed =
+            imputeWithAndWithoutEstimates(
+                data = processedData,
+                processingParameters = processingParameters,
+                imputationParameters = imputationParameters,
+                minObsForEst = 5)
+
+        ## Perform module testing and save back to the database
+
+        imputed %>%
+            checkProductionBalanced(dataToBeSaved = .,
+                                    areaVar = processingParams$areaHarvestedValue,
+                                    yieldVar = processingParams$yieldValue,
+                                    prodVar = processingParams$productionValue,
+                                    conversion = currentFormula$unitConversion) %>%
+            normalise(.) %>%
+            postProcessing(data = .) %>%
+            checkTimeSeriesImputed(dataToBeSaved = .,
+                                   key = c("geographicAreaM49",
+                                           "measuredItemCPC",
+                                           "measuredElement"),
+                                   valueColumn = "Value") %>%
+            filter(flagMethod %in% c("i", "t", "e", "n", "u")) %>%
+            ## NOTE (Michael): This test currently fails occasionally
+            ##                 because it can not overwrite the ('I', '-')
+            ##                 flag for imputed value from the old system.
+            ##                 This is an error in the system as the flag
+            ##                 ('I', '-') should be replaced with ('E', 'e')
+            ##                 which can be over-written.
+            checkProtectedData(dataToBeSaved = .) %>%
+            SaveData(domain = subKey@domain,
+                     dataset = subKey@dataset,
+                     data = .)
+
+        ## Extract the slaughtered number to be synced in the following step.
+        ##
+        ## NOTE (Michael): This function actually normalises data
+        slaughteredAnimal =
+            getSlaughteredAnimal(data = imputed,
+                                 formulaTuples = currentFormula)
+        updatedSlaughteredAnimal =
+            rbind(updatedSlaughteredAnimal, slaughteredAnimal)
+    }
 }
 
-if(!is.null(result)){
+if(!is.null(updatedSlaughteredAnimal)){
     cat("Saving back the updated animal numbers...\n")
     ## Step 3. Copy the slaughtered animal numbers in meat back to the
     ##         animal commodity.
