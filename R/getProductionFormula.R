3fbb885661854fd50fe8ca4f964e2dcdcb2edc18
mk <mkao006@gmail.com>
2016-06-15 10:38:40 +0200

    renamed function

--- R/getProductionFormula.R
+++ R/getProductionFormula.R
@@ -1,22 +1,24 @@
 ##' Get Yield Formula
-##' 
+##'
 ##' This function provides the element codes (for production, yield, and output)
-##' and conversion factor which correspond to the particular item code of 
+##' and conversion factor which correspond to the particular item code of
 ##' interest.
-##' 
-##' @param itemCode The cpc code providing the commodity of interest.  This 
+##'
+##' @param itemCode The cpc code providing the commodity of interest.  This
 ##'   should be a character string.
 ##' @param itemVar The name of the item variable column.
 ##' @param warn Logical.  If FALSE, an error is thrown for missing element
 ##'   types.  If TRUE, it is simply a warning.
-##'   
-##' @return A data.table object providing the relevant element codes.  In some 
-##'   cases, there may be multiple rows, as it is possible to have multiple 
+##'
+##' @return A data.table object providing the relevant element codes.  In some
+##'   cases, there may be multiple rows, as it is possible to have multiple
 ##'   elements which hold production, yield, and output data.
 ##'
 ##' @export
 
-getYieldFormula = function(itemCode, itemVar = "measuredItemCPC", warn = FALSE){
+getProductionFormula = function(itemCode,
+                                itemVar = "measuredItemCPC",
+                                warn = FALSE){
     itemData = GetCodeList(domain = "agriculture", dataset = "aproduction",
                            dimension = itemVar, codes = itemCode)
     itemData = itemData[!is.na(type), ]
@@ -28,9 +30,9 @@ getYieldFormula = function(itemCode, itemVar = "measuredItemCPC", warn = FALSE){
         paste0("item_type IN (",
                paste(paste0("'", as.character(uniqueItemTypes), "'"),
                       collapse = ", "), ")")
-    yieldFormula = ReadDatatable(table = "item_type_yield_elements",
+    productionFormula = ReadDatatable(table = "item_type_yield_elements",
                                  where = condition)
-    if(any(!uniqueItemTypes %in% yieldFormula$item_type)){
+    if(any(!uniqueItemTypes %in% productionFormula$item_type)){
         if(!warn){
             stop("No data in item_type_yield_elements for codes supplied",
                  " (item types are ", paste(uniqueItemTypes, collapse = ", "), ")")
@@ -39,16 +41,16 @@ getYieldFormula = function(itemCode, itemVar = "measuredItemCPC", warn = FALSE){
                  " (item types are ", paste(uniqueItemTypes, collapse = ", "), ")")
         }
     }
-    yieldFormula = merge.data.frame(itemData, yieldFormula,
+    productionFormula = merge.data.frame(itemData, productionFormula,
                                     by.x = "type", by.y = "item_type")
-    yieldFormula = yieldFormula[, c("code", "element_31", "element_41",
+    productionFormula = productionFormula[, c("code", "element_31", "element_41",
                                     "element_51", "factor")]
-    yieldFormula = data.table(yieldFormula)
-    setnames(yieldFormula,
+    productionFormula = data.table(productionFormula)
+    setnames(productionFormula,
              old = c("code", "element_31", "element_41",
                  "element_51", "factor"),
              new = c(itemVar, "input", "productivity",
                  "output", "unitConversion")
              )
-    yieldFormula
+    productionFormula
 }
